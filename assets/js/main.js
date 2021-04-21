66bd7cd9708332777efaf8c793655d29fae65fea
rajshah001 <rajshah30601@gmail.com>
2021-04-21 22:12:58 +0530

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,19 +32,19 @@ $(function() {
 
     var scrollLink = $('.page-scroll');
         // Active link switching
-        $(window).scroll(function() {
-        var scrollbarLocation = $(this).scrollTop();
+    //     $(window).scroll(function() {
+    //     var scrollbarLocation = $(this).scrollTop();
 
-        scrollLink.each(function() {
+    //     scrollLink.each(function() {
 
-          var sectionOffset = $(this.hash).offset().top - 73;
+    //       var sectionOffset = $(this.hash).offset().top - 73;
 
-          if ( sectionOffset <= scrollbarLocation ) {
-            $(this).parent().addClass('active');
-            $(this).parent().siblings().removeClass('active');
-          }
-        });
-    });
+    //       if ( sectionOffset <= scrollbarLocation ) {
+    //         $(this).parent().addClass('active');
+    //         $(this).parent().siblings().removeClass('active');
+    //       }
+    //     });
+    // });
     
     
     //===== Mobile Menu
